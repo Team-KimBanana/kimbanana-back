83e9d43bbcc05615817291f02c315be18f2050b4
yenn222 <20221038@o365.hanbat.ac.kr>
2025-04-07 16:07:23 +0900

    refactor: 패키지 구조 변경

--- src/main/java/io/wisoft/kimbanana/KimbananaApplication.java
+++ src/main/java/io/wisoft/kimbanana/KimbananaApplication.java
@@ -1,13 +1,13 @@
-package kimbanana;
+package io.wisoft.kimbanana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class KimbananaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(KimbananaApplication.class, args);
 	}
 
 }
--- src/test/java/io/wisoft/kimbanana/KimbananaApplicationTests.java
+++ src/test/java/io/wisoft/kimbanana/KimbananaApplicationTests.java
@@ -1,10 +1,10 @@
-package kimbanana;
+package io.wisoft.kimbanana;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class KimbananaApplicationTests {
 
 	@Test
 	void contextLoads() {
