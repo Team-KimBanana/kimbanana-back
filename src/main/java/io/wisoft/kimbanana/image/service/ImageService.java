b20d1443324af23a82053fff8fca6a27b3831d2d
Boyeon-Shin <anna007096@gmail.com>
2025-09-16 17:09:26 +0900

    feat: 이미지 업로드시 DB 저장 처리

--- src/main/java/io/wisoft/kimbanana/image/service/ImageService.java
+++ src/main/java/io/wisoft/kimbanana/image/service/ImageService.java
@@ -1,24 +1,30 @@
 
 package io.wisoft.kimbanana.image.service;
 
-import io.wisoft.kimbanana.image.repository.ImageUploadRepository;
+import io.wisoft.kimbanana.image.entity.SlideImage;
+import io.wisoft.kimbanana.image.repository.JdbcImageUploadRepository;
+import io.wisoft.kimbanana.image.util.ImageExpirationUtils;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import javax.imageio.ImageIO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+@RequiredArgsConstructor
 @Service
 public class ImageService {
-    ImageUploadRepository imageUploadRepository;
+
+    private final JdbcImageUploadRepository imageUploadRepository;
+    private final StorageService storageService;
 
     private static final String UPLOAD_DIR = "uploads/slide-images";
     private static final String URL_PATH_PREFIX = "/slide-images/";
@@ -26,26 +32,30 @@ public class ImageService {
     private static final String UPLOAD_DIR_THUMB = "uploads/presentation-thumbnails";
     private static final String URL_PREFIX_THUMB = "/presentation-thumbnails/";
 
-    public ImageService(final ImageUploadRepository imageUploadRepository) {
-        this.imageUploadRepository = imageUploadRepository;
-    }
-
     public String uploadImage(MultipartFile image) {
         try {
             Files.createDirectories(Paths.get(UPLOAD_DIR));
+
             String fileName = UUID.randomUUID() + ".png";
-          
             Path filePath = Paths.get(UPLOAD_DIR, fileName);
 
-            BufferedImage src = ImageIO.read(image.getInputStream());
-            if (src == null) {
-                throw new IllegalArgumentException("지원하지 않는 이미지 포맷입니다.");
-            }
-
+            BufferedImage src = readImage(image);
             ImageIO.write(src, "png", filePath.toFile());
+            String ImageUrl = URL_PATH_PREFIX + fileName;
 
-            return URL_PATH_PREFIX + fileName;
+            String imageId = UUID.randomUUID().toString();
+            LocalDateTime uploadedAt = LocalDateTime.now();  //추후 expires_at 계산 시 사용하고자 명시적으로 넣음
+            LocalDateTime expiresAt = ImageExpirationUtils.calculateExpirationDate(uploadedAt);
 
+            SlideImage slideImage = SlideImage.builder()
+                    .id(imageId)
+                    .imageUrl(ImageUrl)
+                    .uploadedAt(uploadedAt)
+                    .expiresAt(expiresAt)
+                    .build();
+
+            imageUploadRepository.saveSlideImage(slideImage);
+            return ImageUrl;
         } catch (IOException e) {
             throw new RuntimeException("이미지 업로드 실패" + e);
         }
@@ -58,15 +68,10 @@ public class ImageService {
             Path thumbDir = Paths.get(UPLOAD_DIR_THUMB, presentationId);
             Files.createDirectories(thumbDir);
 
-            clearDirectory(thumbDir);
+            storageService.clearDirectory(thumbDir);
 
             String fileName = UUID.randomUUID() + ".png";
-
-            BufferedImage src = ImageIO.read(image.getInputStream());
-            if (src == null) {
-                throw new IllegalArgumentException("지원하지 않는 이미지 포맷입니다.");
-            }
-
+            BufferedImage src = readImage(image);
             BufferedImage cropped = centerCropToAspect(src, 16, 9);
             BufferedImage resized = resizeImage(cropped, 480, 270);
 
@@ -83,15 +88,15 @@ public class ImageService {
         }
     }
 
-    private void clearDirectory(final Path dir) {
-        if (Files.exists(dir) && Files.isDirectory(dir)) {
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
-                for (Path file : stream) {
-                    Files.deleteIfExists(file);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("디렉토리 정리 실패: " + e.getMessage(), e);
+    private BufferedImage readImage(MultipartFile image) throws IOException {
+        try {
+            BufferedImage src = ImageIO.read(image.getInputStream());
+            if (src == null) {
+                throw new IllegalArgumentException("지원하지 않는 이미지 포맷입니다.");
             }
+            return src;
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 로딩 실패", e);
         }
     }
 
