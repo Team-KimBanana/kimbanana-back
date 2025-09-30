d54bfe6eb13807b63345139cc3798fbc7a027347
Boyeon-Shin <anna007096@gmail.com>
2025-09-30 15:53:46 +0900

    fix: 인증 정보 설정

--- src/main/java/io/wisoft/kimbanana/config/SecurityConfig.java
+++ src/main/java/io/wisoft/kimbanana/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in"dd ).permitAll()
+                        .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
