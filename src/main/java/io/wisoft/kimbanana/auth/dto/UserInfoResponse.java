e34090ee66158980506e99204b03d1176999bdcf
Boyeon-Shin <anna007096@gmail.com>
2025-10-08 13:20:38 +0900

    fix: user id 타입 수정

--- src/main/java/io/wisoft/kimbanana/auth/dto/UserInfoResponse.java
+++ src/main/java/io/wisoft/kimbanana/auth/dto/UserInfoResponse.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UserInfoResponse {
-    private Long id;
+    private String id;
     private String email;
     private String name;
 }
