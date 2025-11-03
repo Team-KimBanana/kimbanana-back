a40082a068b9e601f8eb4e81e544ea401527703c
Boyeon-Shin <anna007096@gmail.com>
2025-11-03 20:26:07 +0900

    feat: 프레젠테이션 참여(join) WebSocket 메시지 핸들러 추가 (#53)

--- src/main/java/io/wisoft/kimbanana/presentation/controller/PresentationWSController.java
+++ src/main/java/io/wisoft/kimbanana/presentation/controller/PresentationWSController.java
@@ -1,17 +1,16 @@
 package io.wisoft.kimbanana.presentation.controller;
 
-import io.wisoft.kimbanana.presentation.dto.response.payload.StructurePayload.SlideStructure;
-import io.wisoft.kimbanana.presentation.entity.Presentation;
+import io.wisoft.kimbanana.presentation.dto.request.ActiveUser;
 import io.wisoft.kimbanana.presentation.entity.Slide;
-import io.wisoft.kimbanana.presentation.dto.response.payload.StructurePayload;
+import io.wisoft.kimbanana.presentation.service.ActiveUserService;
 import io.wisoft.kimbanana.presentation.service.PresentationService;
-import io.wisoft.kimbanana.presentation.util.StructureConvert;
-import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
@@ -19,6 +18,7 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 public class PresentationWSController {
     private final PresentationService presentationService;
+    private final ActiveUserService activeUserService;
 
     @MessageMapping("/slide.edit.presentation.{presentationId}.slide.{currentSlideId}")
     public void editSlide(@DestinationVariable String presentationId,
@@ -30,17 +30,33 @@ public class PresentationWSController {
         presentationService.updateSlide(presentationId, currentSlideId, slide);
     }
 
-    @MessageMapping("/slide.edit.presentation.{presentationId:[^.]+}")
-    public void editSlide(@DestinationVariable String presentationId, Presentation presentation) {
-        log.info("프레젠테이션 구조 편집 요청: {}", presentation.getPresentationId());
 
-        List<SlideStructure> slideStructures = StructureConvert.structureList(presentation.getSlides());
-        StructurePayload response = StructurePayload.builder()
-                .presentationId(presentationId)
-                .slides(slideStructures)
-                .build();
+    @MessageMapping("/presentation.{presentationId}.join")
+    public void joinPresentation(@DestinationVariable String presentationId, SimpMessageHeaderAccessor headerAccessor) {
 
-        presentationService.updateStruct(presentationId, response);
+        Map<String, Object> sessionAttributes = headerAccessor.getSessionAttributes();
+
+        if(sessionAttributes == null) {
+            log.warn("세션 속성 정보를 찾을 수 없음 {}", presentationId);
+            return;
+        }
+
+        String userId = (String) sessionAttributes.get("userId");
+        String userName = (String) sessionAttributes.get("userName");
+        String userType =  (String) sessionAttributes.get("userType");
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("참여한 userId: {}, userNameU: {}, userType: {}, sessionId: {}", userId , userName, userType, sessionId);
+
+        ActiveUser user = new ActiveUser(
+                userId,
+                userName,
+                ActiveUser.UserType.valueOf(userType),
+                sessionId
+        );
+
+        activeUserService.addUser(presentationId, user);
+        log.info("User {} 참여했습니다. presenation {}", userId, presentationId);
     }
 }
 
